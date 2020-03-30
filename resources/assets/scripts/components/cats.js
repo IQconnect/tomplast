60b0c3c35c125284d26cbe80437ce29c2c04f11f
Expenso <kmirosz6@gmail.com>
2020-03-30 14:49:09 +0200

    dropdown

--- resources/assets/scripts/components/cats.js
+++ resources/assets/scripts/components/cats.js
@@ -1,102 +1,107 @@
 const CONFIG = {
-    ELEM: 'data-cats',
-    PRODUCTS: '#products',
-    NAV: '#cat-nav',
-    CLASS: '-is-active',
-}
+  ELEM: 'data-cats',
+  PRODUCTS: '#products',
+  NAV: '#cat-nav',
+  CLASS: '-is-active',
+};
 
-const { ELEM, PRODUCTS, CLASS, NAV } = CONFIG;
+const {
+  ELEM,
+  PRODUCTS,
+  CLASS,
+  NAV,
+} = CONFIG;
 
 const Cats = {
-    init() {
-        this.elem = document.querySelectorAll(`[${ELEM}]`);
-        this.products = document.querySelector(`${PRODUCTS}`);
-        this.navWrapper = document.querySelector(`${NAV}`);
+  init() {
+    this.elem = document.querySelectorAll(`[${ELEM}]`);
+    this.products = document.querySelector(`${PRODUCTS}`);
+    this.navWrapper = document.querySelector(`${NAV}`);
 
-        this.url = this.getPath(window.location.href);
-        this.class = CLASS;
+    this.url = this.getPath(window.location.href);
+    this.class = CLASS;
 
-        if(this.elem.length) {
-            this.topOffset = 300;
-            this.timeOffset = 400;
-            this.setClass();
-            this.addEvents();
+    if (this.elem.length) {
+      this.topOffset = 300;
+      this.timeOffset = 400;
+      this.setClass();
+      this.addEvents();
+    }
+  },
+
+  addEvents() {
+    this.elem.forEach(element => {
+      element.addEventListener('click', e => {
+        e.preventDefault();
+        const $this = e.currentTarget;
+
+        this.setPath($this);
+        if (!$this.classList.contains(this.class)) {
+
+          $('html, body').animate({
+            scrollTop: this.topOffset,
+          }, this.timeOffset);
+          this.products.classList.add(this.class);
+          setTimeout(() => {
+            this.loadProducts();
+          }, this.timeOffset);
         }
-    },
+        this.setClass();
+      });
+    })
+  },
 
-    addEvents() {
-        this.elem.forEach(element => {
-            element.addEventListener('click', e => {
-                e.preventDefault();
-                const $this = e.currentTarget;
+  setClass() {
+    console.log('set Class');
+    this.navWrapper.classList.toggle(this.class);
+    this.elem.forEach(element => {
+      const href = this.getPath(element.getAttribute('href'));
 
-                this.setPath($this);
-                if(!$this.classList.contains(this.class)) {
+      console.log(href, this.url);
+      if (this.url == href) {
+        element.classList.add(this.class);
+        element.parentElement.classList.add(this.class);
+      } else {
+        element.classList.remove(this.class);
+        element.parentElement.classList.remove(this.class);
+      }
+    });
 
-                    $('html, body').animate({ scrollTop: this.topOffset }, this.timeOffset);
-                    this.products.classList.add(this.class);
-                    setTimeout(() => {
-                        this.loadProducts();
-                    }, this.timeOffset);
-                }
-                this.setClass();
-            });
-        })
-    },
+  },
 
-    setClass() {
-        console.log('set Class');
-        this.navWrapper.classList.toggle(this.class);
-        this.elem.forEach(element => {
-            const href = this.getPath(element.getAttribute('href'));
+  setPath($this) {
+    const title = $this.querySelector('h3').innerText;
+    const href = $this.getAttribute('href');
 
-            console.log(href, this.url);
-            if (this.url == href) {
-                element.classList.add(this.class);
-                element.parentElement.classList.add(this.class);
-            }
+    window.history.pushState('', title, href);
 
-            else {
-                element.classList.remove(this.class);
-                element.parentElement.classList.remove(this.class);
-            }
-        });
+    this.url = this.getPath(window.location.href);
+  },
 
-    },
+  getPath(href) {
+    return href.split('/')[href.split('/').length - 2];
+  },
 
-    setPath($this) {
-        const title = $this.querySelector('h3').innerText;
-        const href = $this.getAttribute('href');
+  loadProducts() {
+    this.products.classList.add(this.class);
+    this.products.innerHTML = '';
 
-        window.history.pushState('', title, href);
+    fetch(window.location.href)
+      .then((response) => {
+        return response.text();
+      })
+      .then((html) => {
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(html, 'text/html');
 
-        this.url = this.getPath(window.location.href);
-    },
+        console.log(doc);
 
-    getPath(href) {
-        return href.split('/')[href.split('/').length - 2];
-    },
-
-    loadProducts() {
-        this.products.classList.add(this.class);
-        this.products.innerHTML = '';
-
-        fetch(window.location.href)
-            .then((response) => {
-                return response.text();
-            })
-            .then((html) => {
-                var parser = new DOMParser();
-                var doc = parser.parseFromString(html, 'text/html');
-
-                console.log(doc);
-
-                this.products.innerHTML = doc.querySelector(PRODUCTS).innerHTML;
-            })
-            .then(() => {
-                this.products.classList.remove(this.class);
-            });
-    },
+        this.products.innerHTML = doc.querySelector(PRODUCTS).innerHTML;
+      })
+      .then(() => {
+        this.products.classList.remove(this.class);
+      });
+  },
 }
 
 export default Cats
--- resources/views/layouts/components/contact.blade.php
+++ resources/views/layouts/components/contact.blade.php
@@ -1,29 +1,24 @@
 <section class="section contact">
-    <div class="container">
-        <div class="contact__wrapper">
-            <div class="contact__form">
-                {!! do_shortcode( '[contact-form-7 id="126" title="Formularz 1"]') !!}
-            </div>
-            <div class="contact__info">
-                <h3 class="contact__title major-text">
-                    {{ pll_e('info') }}
-                </h3>
-                @php
-                    $class = "contact__content";
-                    $content = option("contact-contact");
-                @endphp
+  <div class="container">
+    <div class="contact__wrapper">
+      <div class="contact__form">
+        {!! do_shortcode( '[contact-form-7 id="126" title="Formularz 1"]') !!}
+      </div>
+      <div class="contact__info">
+        <h3 class="contact__title major-text">
+          {{ pll_e('info') }}
+        </h3>
+        @php
+          $class = "contact__content";
+          $content = option("contact-contact");
+        @endphp
 
-                <div class="{{ $class }}">
-                    {!! $content !!}
-                </div>
-            </div>
-            <div class="contact__dropdowns">
-                <div>
-                    <h3 class="major-text">
-                        Lorem Ipsum
-                    </h3>
-                </div>
-            </div>
+        <div class="{{ $class }}">
+          {!! $content !!}
         </div>
+      </div>
     </div>
+  </div>
 </section>
+
+@include('components.dropdown')
